09278a97504bec9c1c44bf57acac7dcfa33790b4
Sebastian Niespodziany <sebastianniespodziany@gmail.com>
2025-04-11 11:56:38 +0200

    Remove unused provider block and add provider, outputs, and variables files

--- src/main.tf
+++ src/main.tf
@@ -1,5 +1,3 @@
-provider "local" {}
-
 resource "local_file" "hello_world" {
   filename = "${path.module}/hello_world.txt"
   content  = "Hello, World!"
---
+++ src/provider.tf
@@ -0,0 +1,11 @@
+provider "local" {}
+
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.0"
+    }
+  }
+  required_version = "~>1.8.0"
+}
